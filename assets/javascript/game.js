423ca65b0bfbb939265d58f836efc403b0e47349
Mr_Cheese <kevinprovost776@gmail.com>
2018-12-15 01:17:20 -0600

    index, word generation and variables started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,31 @@ var bandIndex = ["cream", "led zeppelin", "the rolling stones", "heart", "jimi h
 // word generation
 var computerGuess = bandIndex[Math.floor(Math.random() * bandIndex.length)];
 console.log("this is the computer's word: " + computerGuess);
+
+ //Leetters displaying as underscores when unguessed or incorrect
+
+var blanksArray = []
+for (var i=0; i < computerGuess.length; i++) {
+    blanksArray.push("_");
+    console.log(blanksArray)
+}
+
+//starting wins at 0 and adding if user guesses word correctly  
+var wins = 0
+newWinElement = document.createElement("span");
+newWinText = document.createTextNode(" " + wins);
+newWinElement.appendChild(newWinText);
+document.getElementById("wins").append(newWinElement);
+
+var losses = 0;
+newLossesElement = document.createElement("span");
+newLossesText = document.createTextNode(" " + losses);
+newLossesElement.appendChild(newLossesText);
+document.getElementById("losses").append(newLossesElement);
+
+var remainingGuesses = 10;
+newRemainingGuesses = document.createElement("span");
+remainingGuessesText = document.createTextNode(" " + guessesLeft);
+newRemainingGuesses.appendChild(newGuessesText);
+document.getElementById("remaining-guesses").appendChild(newGuessesLeft);
+
