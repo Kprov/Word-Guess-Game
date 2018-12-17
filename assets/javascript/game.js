9675a5f27bea034c277df5a03523cdb130dc6e65
Mr_Cheese <kevinprovost776@gmail.com>
2018-12-16 21:43:51 -0600

    underscores and user guessed letter completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,20 +5,40 @@ var bandIndex = ["cream", "led zeppelin", "the rolling stones", "heart", "jimi h
 var computerGuess = bandIndex[Math.floor(Math.random() * bandIndex.length)];
 console.log("this is the computer's word: " + computerGuess);
 
- //Leetters displaying as underscores when unguessed or incorrect
+//creates underscores for each letter of computerGuess and joins them with a span in the html pulling from the underscoreArray
+var underscoreArray = []; 
+for (var i = 0; i < computerGuess.length; i++) { 
+    underscoreArray.push("_"); 
+    }                               //how to remove from _ from spaces??
+    console.log(underscoreArray); 
+  document.getElementById("bandGuess").innerHTML = underscoreArray.join(" ");
 
-var blanksArray = []
-for (var i=0; i < computerGuess.length; i++) {
-    blanksArray.push("_");
-    console.log(blanksArray)
+//runs function when user releases a key
+var splitWord = computerGuess.split("");
+  console.log(splitWord)
+var guessedLetters = [];
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    console.log("user guess: " + userGuess)
+    if (guessedLetters.indexOf(userGuess) >=0){
+        guessedLetters.push(userGuess);
+        document.getElementById("currentguesses").innerhtml = guessedletters.join(" ");
+    }
 }
+    
+    
 
-//starting wins at 0 and adding if user guesses word correctly  
+
+
+  //starting wins at 0 and adding if user guesses word correctly  
 var wins = 0
 newWinElement = document.createElement("span");
 newWinText = document.createTextNode(" " + wins);
 newWinElement.appendChild(newWinText);
 document.getElementById("wins").append(newWinElement);
+
+//starting losses at 0 and adding if user guesses incorrectly
 
 var losses = 0;
 newLossesElement = document.createElement("span");
@@ -26,9 +46,11 @@ newLossesText = document.createTextNode(" " + losses);
 newLossesElement.appendChild(newLossesText);
 document.getElementById("losses").append(newLossesElement);
 
-var remainingGuesses = 10;
-newRemainingGuesses = document.createElement("span");
-remainingGuessesText = document.createTextNode(" " + guessesLeft);
-newRemainingGuesses.appendChild(newGuessesText);
-document.getElementById("remaining-guesses").appendChild(newGuessesLeft);
+// //starting remaining guesses at 10 and dropping by 1 for each letter guessed
+
+// var remainingGuesses = 10;
+// newRemainingGuesses = document.createElement("span");
+// remainingGuessesText = document.createTextNode(" " + remainingGuesses);
+// newRemainingGuesses.appendChild(remainingGuessesText);
+// document.getElementById("remaining-guesses").appendChild(newRemainingGuesses);
 
